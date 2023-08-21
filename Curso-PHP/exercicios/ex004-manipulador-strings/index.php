edccb48c81d7c00448505f80ae0ff6db4d512cd9
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2023-08-21 11:12:56 -0300

    Create index.php

---
+++ Curso-PHP/exercicios/ex004-manipulador-strings/index.php
@@ -0,0 +1,9 @@
+<?php 
+    $nome = "Anderson";
+    $sobrenome = "Roldão";
+
+    // com interpretação
+    echo "Olá $nome $sobrenome \u{1f596}<br>"; 
+    // sem interpretação
+    echo 'Olá $nome $sobrenome \u{1f596}';
+?>
