19fc0b2350212b5df3512b48032c6482703090b4
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2023-08-25 15:17:49 -0300

    Create index.php

---
+++ Curso-PHP/exercicios/ex006-op-aritméticos/index.php
@@ -0,0 +1,10 @@
+<?php 
+    $resultado = "2" + "2"; // Soma
+    $resultado1 = "2" . "2"; // Concatenação
+    
+    var_dump($resultado, $resultado1);
+
+    $resultado2 = 50 / 2 + 3 **2;
+
+    echo "<br><br>O resultado é $resultado2";
+?>
