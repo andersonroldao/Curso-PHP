19433c1e7b3dbf00036232ca50d84e2d67821362
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2023-09-04 14:51:11 -0300

    Create form.php

---
+++ Curso-PHP/desafios/004-conversor-moeda-v2/form.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Conversor de Moedas V.1</title>
+</head>
+<body>
+    <header>
+        <h1>Conversor de Real (BRL) para Dollar (USD)</h1>
+    </header>
+
+    <main>
+        <?php
+            $cotacao = 5.05;
+            $real = $_GET["numero"];
+            if (empty($real)) {
+                $real = 0;
+            }
+            //$real = str_replace(',','.', $real);
+            $dolar = $real / $cotacao;
+
+            //Number Format
+            /* $dolar = number_format($dolar, 2,',',".");
+            $real = number_format($real, 2,',',".");
+
+            echo "Seus R$ $real, equivalem a US$ $dolar*<br>"; */
+
+            //Biblioteca intl (Internalization)
+            $moeda = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+            echo "<h2>Seus " . numfmt_format_currency($moeda, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($moeda, $dolar, "USD") . "</strong>*</h2>";
+        ?>
+
+        <p class="nota">*Cotação obtida através do <a href="https://www.bcb.gov.br/estabilidadefinanceira/fechamentodolar" target="_blank">Banco Central do Brasil</a>.</p>
+
+        <p class="nota">*Para dias não úteis, assume-se a cotação do dia útil imediatamente anterior.</p>
+
+        <input type="button" value="Voltar" id="enviar" onclick="javascript:history.go(-1)">
+    </main>
+</body>
+</html>
