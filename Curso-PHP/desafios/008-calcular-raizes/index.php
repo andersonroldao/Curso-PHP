646dd8ce1d0a486c018e8e7f7e2b866016ec3850
Anderson Roldão <66678707+andersonroldao@users.noreply.github.com>
2023-09-14 14:50:19 -0300

    Update index.php

--- Curso-PHP/desafios/008-calcular-raizes/index.php
+++ Curso-PHP/desafios/008-calcular-raizes/index.php
@@ -15,13 +15,16 @@
     <main>
         <?php 
             $numero = $_GET["numero"];
+            if ($numero == "") {
+                $numero = 0;
+            }
         ?>
 
         <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
             <h2>Informe um número</h2>
 
             <label for="numero">Número:</label>
-            <input type="number" name="numero" id="idnumero" min="1" value="<?=$numero?>">
+            <input type="number" name="numero" id="idnumero" min="0" value="<?=$numero?>">
 
             <input type="submit" value="Calcular Raízes" id="enviar">
         </form>
@@ -35,14 +38,16 @@
             $raizCubica = $numero ** (1 / 3);
         ?>
 
-        <p>Analisando o <strong>número <?=number_format($numero, 0, ".", ",")?></strong>, temos:</p>
+        <p>
+            Analisando o <strong>número <?=number_format($numero, 0, ",", ".")?></strong>, temos:
+        </p>
 
         <ul>
             <li>
-                A sua raiz quadrada é <strong><?=number_format($raizQuadrada, 3, ".", ",")?></strong>.
+                A sua raiz quadrada é <strong><?=number_format($raizQuadrada, 3, ",", ".")?></strong>.
             </li>
             <li>
-                A sua raiz cúbica é <strong><?=number_format($raizCubica, 3, ".", ",")?></strong>.
+                A sua raiz cúbica é <strong><?=number_format($raizCubica, 3, ",", ".")?></strong>.
             </li>
         </ul>
     </section>
